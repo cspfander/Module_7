3a23467a27ad8180564ffc45a63876b5a5407228
packe <cspfander@dmacc.edu>
2019-10-07 10:01:54 -0500

    3/4 Tests Fail - Fixed some test coding

--- test_collections/test_basic_list_exception.py
+++ test_collections/test_basic_list_exception.py
@@ -4,21 +4,21 @@ import fun_with_collections.basic_list as topic1
 
 
 class TestList(unittest.TestCase):
-    @patch('fun_with_collections.basic_list.get_input', return_value='5')
+    @patch('fun_with_collections.basic_list_exception.get_input', return_value='5')
     def test_make_list(self, input):
         self.assertEqual(topic1.make_list(), [5, 5, 5])
 
-    @patch('fun_with_collections.topic1.get_input', return_value='ab')  # patch function for input
+    @patch('fun_with_collections.basic_list_exception.make_list', return_value='ab')  # patch function for input
     def test_make_list_non_numeric(self, input):                        # pass input
         with self.assertRaises(ValueError):                             # this is familiar
             topic1.make_list()                                          # call the function!
 
-    @patch('fun_with_collections.topic1.get_input', return_value=-1)
+    @patch('fun_with_collections.basic_list_exception.make_list', return_value=-1)
     def test_make_list_below_range(self, input):
         with self.assertRaises(ValueError):
             topic1.make_list()
 
-    @patch('fun_with_collections.topic1.get_input', return_value=51)
+    @patch('fun_with_collections.basic_list_exception.make_list', return_value=51)
     def test_make_list_above_range(self, input):
         with self.assertRaises(ValueError):
             topic1.make_list()
