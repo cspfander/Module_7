bbaef2d05adc15d8db844c2a3584a8a235ccbf5d
packe <cspfander@dmacc.edu>
2019-10-07 14:29:38 -0500

    2/3 Tests pass, unable to get sort to work for array

--- fun_with_collections/sort_and_search_array.py
+++ fun_with_collections/sort_and_search_array.py
@@ -6,16 +6,25 @@ Last date modified: 10/7/2019
 The purpose of this program is to write 2 functions sort_array() and search_array() that will return the index of the
 object in the list or -1 if the item is not found as well as sort the array.
 """
-import array as arr
+from array import array as arr
 
 
-def sort_array(generic_array):
-    pass
+def sort_array(array):
+    list_1 = array.tolist()
+    sorted_list = list_1.sort()
+    b = arr('i', sorted_list)
+    return b
 
 
 def search_array(generic_array):
-    pass
+    try:
+        index_input = int(input("Please enter a number you wish to search in the array: "))
+        index = generic_array.index(index_input)
+        return 'The index of ' + str(index_input) + ": " + str(index)
+    except ValueError:
+        return "-1"
 
 
 if __name__ == '__main__':
-    pass
+    a = arr('i', [3, 1, 2])
+    print(sort_array(a))
--- test_collections/test_sort_and_search_array.py
+++ test_collections/test_sort_and_search_array.py
@@ -5,15 +5,15 @@ import fun_with_collections.sort_and_search_array as topic1
 
 class MyTestCase(unittest.TestCase):
     def test_sort_list(self):
-        generic_array = arr.array('i', 3, 1, 2)
+        generic_array = arr.array('i', [3, 1, 2])
         self.assertEqual(topic1.sort_array(generic_array), [1, 2, 3])
 
     def test_search_list_good(self):
-        generic_array = arr.array('i', 3, 1, 2)
+        generic_array = arr.array('i', [3, 1, 2])
         self.assertEqual(topic1.search_array(generic_array), "The index of 3: 0")
 
     def test_search_list_bad(self):
-        generic_array = arr.array('i', 3, 1, 2)
+        generic_array = arr.array('i', [3, 1, 2])
         self.assertEqual(topic1.search_array(generic_array), "-1")
 
 
