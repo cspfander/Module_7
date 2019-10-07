9baf284d4bf5b5838173bc4b64a59932f9e6ce88
packe <cspfander@dmacc.edu>
2019-10-07 13:36:50 -0500

    Initial Commit - All tests fail

---
+++ fun_with_collections/sort_and_search_array.py
@@ -0,0 +1,21 @@
+"""
+Program: sort_and_search_array.py
+Author: Colten Pfander
+Last date modified: 10/7/2019
+
+The purpose of this program is to write 2 functions sort_array() and search_array() that will return the index of the
+object in the list or -1 if the item is not found as well as sort the array.
+"""
+import array as arr
+
+
+def sort_array(generic_array):
+    pass
+
+
+def search_array(generic_array):
+    pass
+
+
+if __name__ == '__main__':
+    pass
---
+++ test_collections/test_sort_and_search_array.py
@@ -0,0 +1,21 @@
+import unittest
+import array as arr
+import fun_with_collections.sort_and_search_array as topic1
+
+
+class MyTestCase(unittest.TestCase):
+    def test_sort_list(self):
+        generic_array = arr.array('i', 3, 1, 2)
+        self.assertEqual(topic1.sort_array(generic_array), [1, 2, 3])
+
+    def test_search_list_good(self):
+        generic_array = arr.array('i', 3, 1, 2)
+        self.assertEqual(topic1.search_array(generic_array), "The index of 3: 0")
+
+    def test_search_list_bad(self):
+        generic_array = arr.array('i', 3, 1, 2)
+        self.assertEqual(topic1.search_array(generic_array), "-1")
+
+
+if __name__ == '__main__':
+    unittest.main()
