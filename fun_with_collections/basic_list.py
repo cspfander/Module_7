6b1470c4d8226291ac57372011c77356f544adb6
packe <cspfander@dmacc.edu>
2019-10-07 09:21:54 -0500

    Initial Commit - 1 Test Failed

---
+++ fun_with_collections/basic_list.py
@@ -0,0 +1,20 @@
+"""
+Program: basic_list.py
+Author: Colten Pfander
+Last date modified: 10/7/2019
+
+The purpose of this program is to declare two functions that will eventually return a list of user input and another
+to return just one input.
+"""
+
+
+def make_list():
+    pass
+
+
+def get_input():
+    pass
+
+
+if __name__ == '__main__':
+    print()
