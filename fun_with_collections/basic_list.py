37b77d35fa1ec69d4a96a62d52f74e5b2934e711
packe <cspfander@dmacc.edu>
2019-10-07 09:46:21 -0500

    Test Passed

--- fun_with_collections/basic_list.py
+++ fun_with_collections/basic_list.py
@@ -9,11 +9,18 @@ to return just one input.
 
 
 def make_list():
-    pass
+    input_list_of_three = []
+    try:
+        for value in range(0, 3):
+            num = int(get_input())
+            input_list_of_three.append(num)
+        return input_list_of_three
+    except ValueError:
+        print("Error! Please enter only numbers!")
 
 
 def get_input():
-    pass
+    return input("Please enter a number: ")
 
 
 if __name__ == '__main__':
