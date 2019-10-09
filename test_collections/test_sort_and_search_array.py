8c06108967e256e3ca24882204fd2441b73c5266
packe <cspfander@dmacc.edu>
2019-10-09 03:32:14 -0500

    All Tests Pass! Altered function code and simplified tests

--- test_collections/test_sort_and_search_array.py
+++ test_collections/test_sort_and_search_array.py
@@ -6,15 +6,15 @@ import fun_with_collections.sort_and_search_array as topic1
 class MyTestCase(unittest.TestCase):
     def test_sort_list(self):
         generic_array = arr.array('i', [3, 1, 2])
-        self.assertEqual(topic1.sort_array(generic_array), [1, 2, 3])
+        self.assertEqual(topic1.sort_array(generic_array), arr.array('i', [1, 2, 3]))
 
     def test_search_list_good(self):
         generic_array = arr.array('i', [3, 1, 2])
-        self.assertEqual(topic1.search_array(generic_array), "The index of 3: 0")
+        self.assertEqual(topic1.search_array(generic_array, 3), "The index of 3: 0")
 
     def test_search_list_bad(self):
         generic_array = arr.array('i', [3, 1, 2])
-        self.assertEqual(topic1.search_array(generic_array), "-1")
+        self.assertEqual(topic1.search_array(generic_array, 5), "-1")
 
 
 if __name__ == '__main__':
